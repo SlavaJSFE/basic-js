d928df4d695579941c8d002ada05ede02f9b8292
Viachaslau <viachaslau.shpileuski@gmail.com>
2020-10-30 10:51:33 +0300

    feat: some changes is two tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -15,34 +15,4 @@ module.exports = class DepthCalculator {
     counter = 1;
     return depth;
     }
-  // calculateDepth(arr) {
-  //   if (arr instanceof Array) {
-  //     let maximal = 0;
-  //     for (let i = 0; i < arr.length; i++) {
-  //       let cur = this.calculateDepth.call(this.arr[i]);
-  //       if (maximal < cur) {
-  //         maximal = cur;
-  //       }
-  //     }
-  //     return 1 + maximal;
-  //   } else {
-  //     return 0;
-  //   }
-  // }
-
-  // calculateDepth(arr) {
-  //   let counter = 1;
-  //   let depthCount = 1;
-  //   for (let item of arr) {
-  //     if (Array.isArray(item)) {
-  //       counter = 1 + this.calculateDepth(item);   
-  //       this.calculateDepth(item);
-  //     }
-  //   }    
-  //   if (this.counter > this.depthCount) {
-  //     this.depthCount = this.counter;
-  //   }
-  //   this.counter = 1;
-  //   return this.depthCount;
-  // }
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,22 +2,101 @@ const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
   getLength() {
-    
+    if (this.chain !== undefined) {
+      return this.chain.length;
+    }
   },
+
   addLink(value) {
-    
+    let link = '';
+    if (value !== 'undefined') {
+      link = value;
+    } else if (value === null) {
+      link = 'null';
+    } else if (this.chain === undefined) {
+      this.chain = [link];
+      return this;
+    } else {
+      this.chain.push(link);
+      return this;
+    }
   },
+
   removeLink(position) {
-    
+    if (this.chain === undefined) {
+      this.chain = undefined;
+      throw new Error;
+    } else {
+      if (typeof position === 'number' || !Number.isInteger(position) || position < 1 || position > this.chain.length) {
+        this.chain = undefined;
+        throw new Error;
+      } else {
+        this.chain.splice(position - 1, 1);
+        return this;
+      }
+    }
   },
+
   reverseChain() {
-    
+    if (this.chain !== undefined) {
+      this.chain.reverse();
+      return this;
+    }
   },
+
   finishChain() {
-    
+    if (this.chain !== undefined) {
+      const result = `( ${this.chain.join(' )~~( ')} )`;
+      this.chain = undefined;
+      return result;
+    }
   }
 };
 
-
+// const chainMaker = {
+//   getLength() {
+//     if (this.chain !== undefined)
+//       return this.chain.length
+//   },
+//   addLink(value) {
+//     let link = ''
+//     if (value !== 'undefined'){
+//       link = value
+//     }
+//     if (value === null) link = 'null'
+//     if (this.chain === undefined) {
+//       this.chain = [link]
+//       return this
+//     } else {
+//       this.chain.push(link)
+//       return this
+//     }
+//   },
+//   removeLink(position) {
+//     if (this.chain === undefined) {
+//       this.chain = undefined
+//       throw Error
+//     } else {
+//       if (typeof position !== 'number' ||
+//           !Number.isInteger(position) ||
+//           position < 1 ||
+//           position > this.chain.length) {
+//             this.chain = undefined
+//             throw Error
+//           }
+//       this.chain.splice(position - 1, 1)
+//       return this
+//     }
+//   },
+//   reverseChain() {
+//     if (this.chain !== undefined) this.chain.reverse()
+//     return this
+//   },
+//   finishChain() {
+//     const result = `( ${this.chain.join(' )~~( ')} )`
+//     this.chain = undefined
+//     return result
+//   }
+// };
 
 module.exports = chainMaker;
