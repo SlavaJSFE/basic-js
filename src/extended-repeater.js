053bab7d4bd5d0fc13038bdd9468d2b86eb72b0b
Viachaslau <viachaslau.shpileuski@gmail.com>
2020-11-01 21:59:04 +0300

    feat: solve repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  
-  // remove line with error and write your code here
+  let result = '';
+  if (!options.separator) {
+    options.separator = '+';
+  }
+  if (!options.additionSeparator) {
+    options.additionSeparator = '|';
+  }
+  if (options.addition + '' && options.addition !== undefined) {
+    result = (options.addition + '' + options.additionSeparator).repeat(options.additionRepeatTimes - 1) + options.addition;
+  }
+  return (str + result + options.separator).repeat(options.repeatTimes - 1) + str + result;
 };
   
